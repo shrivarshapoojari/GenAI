ef73ee6f99bae3c356cf60c93268764f6456884b
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-11-09 12:46:17 +0530

    simple chatbot using groq

--- LangChain/LLMProjects/serve.py
+++ LangChain/LLMProjects/serve.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
@@ -13,3 +13,31 @@ groq_api_key = os.getenv("GROQ_API_KEY")
 model=ChatGroq(model="Gemma2-9b-It", api_key=groq_api_key)
 
 
+
+generic_template="Translate the following to {language}"
+
+
+
+prompt=ChatPromptTemplate.from_messages([
+        ("system",generic_template),("user","{text}")
+])
+
+
+parser=StrOutputParser()
+
+
+chain=prompt|model|parser
+
+
+app=FastAPI(title="LangChain",description="A language translation API",version="0.1.0")
+
+add_routes(
+   app,
+   chain,
+   path="/chain"
+)
+
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="localhost",port=8000)
